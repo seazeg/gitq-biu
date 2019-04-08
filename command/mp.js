a908aade0d067d0c72197f5e825df1cdc96bd928
Evan.G <seazeg@126.com>
2019-04-08 16:54:21 +0800

    update command  :art:

--- command/mp.js
+++ command/mp.js
@@ -10,37 +10,35 @@ module.exports = (program) => {
         program.message = `Default Commit Message :art:`
     }
 
-    const currBranch = shell.exec(`git symbolic-ref --short -q HEAD`, {
-        silent: true
-    }).stdout
+    let currBranch = getCurBranch()
 
-    console.log(program.branch);
-    const commands = {
+    // console.log(program.branch);
+    let commands = {
         1: `git add -A`,
         2: `git commit -m '${program.message}'`,
         3: `git pull`,
         4: `git push`,
         5: `git checkout ${program.branch}`,
         6: `git pull`,
-        7: `git merge '${currBranch}'`,
-        8: `git commmit -m '${program.message}'`,
-        9: `git push'`,
-        10: `git checkout ${currBranch}`
+        7: `git merge ${currBranch}`,
+        8: `git push`,
+        9: `git checkout ${currBranch}`
     }
 
     for (let i in commands) {
         const exec = shell.exec(commands[i], {
             silent: true
         })
+        currBranch = getCurBranch();
         if (exec.code !== 0) {
-            console.log(chalk.red(`>> [ ${commands[i]} ] `));
+            console.log(chalk.gray(`● ${currBranch} `), chalk.red(`>> [ ${commands[i]} ] `))
             console.log(chalk.red(`====================== Error ======================\n`));
             console.log(chalk.red(exec.stdout));
             console.log(chalk.red(exec.stderr));
             console.log(chalk.red(`====================== Error ======================`));
             if (program.closed === undefined) {
                 if (exec.stderr.includes(`Automatic merge failed`) || exec.stdout.includes(`Automatic merge failed`)) {
-                    mergeTool()
+                    mergeTool();
                 } else {
                     shell.exit(1);
                 }
@@ -49,10 +47,16 @@ module.exports = (program) => {
             }
 
         } else {
-            console.log(chalk.green(`>> [ ${commands[i]} ] `), chalk.green(`Successfully`));
+            console.log(chalk.gray(`● ${currBranch} `), chalk.green(`>> [ ${commands[i]} ] `), chalk.green(`Successfully`));
             !exec.stdout ? console.log(exec.stderr) : console.log(exec.stdout);
         }
     }
+}
+
+function getCurBranch() {
+    return shell.exec(`git symbolic-ref --short -q HEAD`, {
+        silent: true
+    }).stdout
 }
 
 function mergeTool() {
--- command/push.js
+++ command/push.js
@@ -9,27 +9,30 @@ module.exports = (program) => {
     if (!program.message) {
         program.message = `Default Commit Message :art:`
     }
-    const commands = {
+
+    let currBranch = getCurBranch()
+
+    let commands = {
         1: `git add -A`,
         2: `git commit -m '${program.message}'`,
         3: `git pull`,
         4: `git push`
-        // 5: `git --version`
     }
 
     for (let i in commands) {
         const exec = shell.exec(commands[i], {
             silent: true
         })
+        currBranch = getCurBranch();
         if (exec.code !== 0) {
-            console.log(chalk.red(`>> [ ${commands[i]} ] `));
+            console.log(chalk.gray(`● ${currBranch} `), chalk.red(`>> [ ${commands[i]} ] `))
             console.log(chalk.red(`====================== Error ======================\n`));
             console.log(chalk.red(exec.stdout));
             console.log(chalk.red(exec.stderr));
             console.log(chalk.red(`====================== Error ======================`));
             if (program.closed === undefined) {
                 if (exec.stderr.includes(`Automatic merge failed`) || exec.stdout.includes(`Automatic merge failed`)) {
-                    mergeTool()
+                    mergeTool();
                 } else {
                     shell.exit(1);
                 }
@@ -38,10 +41,15 @@ module.exports = (program) => {
             }
 
         } else {
-            console.log(chalk.green(`>> [ ${commands[i]} ] `), chalk.green(`Successfully`));
-            !exec.stdout ? console.log(exec.stderr) : console.log(exec.stdout);
+            console.log(chalk.gray(`● ${currBranch} `), chalk.green(`>> [ ${commands[i]} ] `), chalk.green(`Successfully`));!exec.stdout ? console.log(exec.stderr) : console.log(exec.stdout);
         }
     }
+}
+
+function getCurBranch() {
+    return shell.exec(`git symbolic-ref --short -q HEAD`, {
+        silent: true
+    }).stdout
 }
 
 function mergeTool() {
