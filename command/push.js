564e87fa673a650cfb085b959d263b1e78ad7142
user.email <seazeg@126.com>
2020-04-30 15:42:37 +0800

    update author - user.email

--- command/push.js
+++ command/push.js
@@ -1,21 +1,30 @@
 'use strict'
+const shell = require('shelljs');
 const chalk = require('chalk')
 const path = require('path');
 const handler = require('../utils/handler')
 
 console.log((chalk.blue(`\n Current Directory > ` + path.resolve('./') + `\n `)));
+
+
+const getAuthor = () => {
+    return shell.exec(`git config --global user.name`, {
+        silent: true
+    }).stdout
+}
+
+
 module.exports = (program) => {
     if (!program.message) {
-        program.message = `Default Commit Message`
+        program.message = `default Commit Message - ${getAuthor()}`
     }
 
     let commands = {
         1: `git add -A`,
-        2: `git commit -m '${program.message}'`,
+        2: `git commit -m "${program.message} - ${getAuthor()}"`,
         3: `git pull`,
         4: `git push`
     }
 
     handler(commands, program)
 }
-
