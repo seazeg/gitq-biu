84fda8b2629f1e91c16716d5e869d74a7f013106
Evan.G <seazeg@126.com>
2020-04-30 15:52:34 +0800

    default Commit Message [Evan.G] [Evan.G]

--- command/push.js
+++ command/push.js
@@ -15,7 +15,7 @@ const getAuthor = () => {
 
 module.exports = (program) => {
     if (!program.message) {
-        program.message = `default Commit Message [${getAuthor()}]`
+        program.message = `default Commit Message [${getAuthor().trim()}]`
     }
 
     let commands = {
