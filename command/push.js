57324e4f8b74d7547671e0b974a339be25c19c9a
Evan.G <seazeg@126.com>
2020-04-30 14:54:13 +0800

    Default Commit Message

--- command/push.js
+++ command/push.js
@@ -6,12 +6,12 @@ const handler = require('../utils/handler')
 console.log((chalk.blue(`\n Current Directory > ` + path.resolve('./') + `\n `)));
 module.exports = (program) => {
     if (!program.message) {
-        program.message = `Default Commit Message`
+        program.message = 'Default Commit Message'
     }
 
     let commands = {
         1: `git add -A`,
-        2: `git commit -m '${program.message}'`,
+        2: `git commit -m "${program.message}"`,
         3: `git pull`,
         4: `git push`
     }
